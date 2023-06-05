9310621f00e948cdfcd401d29527236031a76633
AssFerj <assisjuniorcam@gmail.com>
2023-06-05 20:57:30 -0300

    install cors e dotenv and config project to deploy

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import { userRoutes } from './routes/user.routes';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/users', userRoutes());
 
-app.listen(3333, ()=>{
-    console.log('API is running on port: 3333')
+app.listen(process.env.PORT, ()=>{
+    console.log('API is running on port: ' + process.env.PORT)
 });
 
